6bf69f3e0e43e52a3521d7a58736e77e5f62e095
whowouldwin <leranetwork@gmail.com>
2025-07-19 22:19:10 +0300

    refactor: move ResultList.test.tsx.snap

--- rs-react-app/src/__tests__/rickAndMorty.test.tsx
+++ rs-react-app/src/__tests__/rickAndMorty.test.tsx
@@ -1,7 +1,11 @@
 import { describe, it, expect, beforeAll, afterAll, afterEach } from 'vitest';
 import { http, HttpResponse } from 'msw';
 import { setupServer } from 'msw/node';
-import { fetchAll, type ApiResponse, type Character } from '../api/rickAndMorty.ts';
+import {
+  fetchAll,
+  type ApiResponse,
+  type Character,
+} from '../api/rickAndMorty.ts';
 import { FetchError } from '../error/FetchError.ts';
 
 const mockCharacters: Character[] = [
