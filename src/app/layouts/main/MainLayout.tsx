55d8326343738e907b50abd4fa2f041da2159813
Valeryia Bessonava <leranetwork@gmail.com>
2025-08-14 23:44:11 +0300

    refactor: remove unused field

--- src/app/layouts/main/MainLayout.tsx
+++ src/app/layouts/main/MainLayout.tsx
@@ -4,11 +4,8 @@ import { Outlet } from 'react-router-dom';
 import { Header } from '@/features/site-header/Header';
 import { Flyout } from '@/widgets/selected-items-flyout/Flyout';
 
-import type { Character } from '@/utils/types/rickAndMorty';
-
 interface MainLayoutProps {
   handleSearch: (query: string) => void;
-  characters: Character[];
 }
 
 export const MainLayout: FC<MainLayoutProps> = ({ handleSearch }) => {
